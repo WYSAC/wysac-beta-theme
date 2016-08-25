b8200be36663e92946f760b88807a2ff606b6fa7
jlschillinger <jessica.schillinger@uwyo.edu>
2016-08-25 11:20:38 -0600

    Starting the layout
    bootstrap for masthead, topbar created, moving nav, inserting logo,
    site-branding and titles.

--- wp-content/themes/wysac-2016/header.php
+++ wp-content/themes/wysac-2016/header.php
@@ -22,31 +22,59 @@
 <?php wp_head(); ?>
 </head>
 
-<body class="container-fluid">
+<body>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'wysac-beta' ); ?></a>
+	<!--Need a Screenreader skip link here-->
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+		<!-- TOP BAR
+		=================================== -->
+		<div class="topbar">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<a href="http://www.uwyo.edu/" class="uw-linkback">University of Wyoming</a>
+					</div>
+					<div class="col-md-6">
+						<a href="http://www.uwyo.edu/" ><img src="http://placehold.it/75x25" class="pull-right" /></a>
+					</div>
+				</div> <!--.row-->
+			</div><!--.container-->
+		</div><!--.topbar-->
+		<!-- Site-Logo + Navigation
+		=================================== -->
+		<div class="container site-branding">
+			<div class="row">
+				<div class="col-md-2">
+					<div class="site-logo">
+						<a href="#"><img src="http://placehold.it/150x150" /></a>
+					</div> <!--.site-logo-->
+				</div><!--.col-md-2-->
+				<div class="col-md-10">
+					<nav id="site-navigation" class="main-navigation" role="navigation">
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					</nav><!-- #site-navigation -->
+				</div><!--.col-md-10-->
+			</div><!--.row-->
+		<!-- Site-Branding
+		=================================== -->
+			<div class="row">
+				<div class="col-md-12">
+							<?php if ( is_front_page() && is_home() ) : ?>
+									<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php else : ?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						<?php
+						endif;
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wysac-beta' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+						$description = get_bloginfo( 'description', 'display' );
+						if ( $description || is_customize_preview() ) : ?>
+							<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+						<?php
+						endif; ?>
+				</div> <!--.col-md-12-->
+			</div><!--.row-->
+		</div><!--.container .site-branding-->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
